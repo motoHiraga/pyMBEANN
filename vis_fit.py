548adf83b3dabce5e5ec3dbd8592787b01767428
motoHiraga <100577843+motoHiraga@users.noreply.github.com>
2022-02-28 21:28:17 +0900

    Add files via upload

---
+++ vis_fit.py
@@ -0,0 +1,69 @@
+'''
+Example for visualizing the fitness transition throughout the evolutionary process.
+'''
+
+import os
+import pickle
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+import pandas as pd
+
+
+path = os.path.join(os.path.dirname(__file__), 'examples/xor/results_xor_2147483648')
+# path = os.path.join(os.path.dirname(__file__), 'examples/cart2pole/results_cart_0')
+
+logs = []
+with open('{}/log_stats.pkl'.format(path), 'rb') as pkl:
+    while True:
+        try:
+            o = pickle.load(pkl)
+        except EOFError:
+            break
+        logs.append(o)
+
+df = pd.DataFrame(logs[1:], columns=logs[0])
+
+print(df)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+# ax = df.plot(x='Gen', colormap='viridis', linewidth=3.0)
+
+plt_mean = plt.plot(df['Gen'], df['Mean'],
+                    linewidth=2.0,
+                    color='cornflowerblue',
+                    label='mean')
+
+plt_std = plt.fill_between(df['Gen'],
+                           df['Mean'] - df['Std'],
+                           df['Mean'] + df['Std'],
+                           color='cornflowerblue',
+                           alpha=0.4,
+                           linewidth=0,
+                           label='standard deviation \nof mean')
+
+plt_max = plt.plot(df['Gen'], df['Max'],
+                   linewidth=2.0,
+                   linestyle='--',
+                   color='g',
+                   label='max')
+
+plt_min = plt.plot(df['Gen'], df['Min'],
+                   linewidth=2.0,
+                   linestyle=':',
+                   color='y',
+                   label='min')
+
+ax.tick_params(labelsize=14)
+ax.set_xlabel(r'Generation', fontsize=18)
+ax.set_ylabel(r'Fitness Value', fontsize=18)
+ax.legend(fontsize=12)
+# ax.legend(loc='lower right', fontsize=12)
+
+
+plt.tight_layout()
+plt.savefig('{}/mbeann_fitness.pdf'.format(path))
+plt.show()
