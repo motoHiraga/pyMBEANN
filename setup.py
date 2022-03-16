ccd110b5a59f8b1b35e39453a9f4af5e26ca43d0
motoHiraga <100577843+motoHiraga@users.noreply.github.com>
2022-03-16 18:46:49 +0900

    Add setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='mbeann',
+    version='0.1',
+    author='Motoaki Hiraga',
+    author_email='100577843+motoHiraga@users.noreply.github.com',
+    url='https://github.com/motoHiraga/pyMBEANN',
+    license='MIT',
+    description='An implementation of Mutation-Based Evolving Artificial Neural Network (MBEANN)',
+    py_modules=['mbeann'],
+    install_requires=['numpy', 'networkx', 'matplotlib',],
+    extras_require={'examples': ['pandas']}
+)
